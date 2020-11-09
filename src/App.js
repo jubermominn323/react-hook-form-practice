c937d5fd02cf57cafd4b20a1b700fb1ffe4eac00
jubermomin323 <jubermomin323@gmail.com>
2020-11-09 23:51:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import { useForm } from 'react-hook-form'
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    email:"",
+    password:""  
+  })
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = (data) =>{
+    console.log(data)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-constrol">
+          <label>Email:</label>
+          <input type="text"
+            name="email"
+            ref = {register({
+              required:"Email is required",
+              pattern: {
+                value : /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message:"Email must be valid"
+              }
+            })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div className="form-constrol">
+          <label>Password:</label>
+          <input type="password"
+            name="password"
+            ref = {register({
+              required: "Passsword is required",
+              minLength:{
+                value: 6,
+                message:"Password should be atleast 6 character long"
+              }
+            })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
   );
 }
